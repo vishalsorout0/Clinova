370da9899a9b1899d8d49dddfa8692ed451e3463
unknown <soroutvishal9@gmail.com>
2026-09-19 20:53:13 +0530

    connected with backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,5 @@
 const API_BASE_URL =
-  "http://127.0.0.1:8000/api";
-  // import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api";
+  import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api";
 
 export async function apiRequest(
   endpoint,
